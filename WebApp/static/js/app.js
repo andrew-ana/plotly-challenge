6b27556961ba80fe6464438eb466e9a895920a75
Andrew Anastasiades <atanasta.aa@gmail.com>
2021-06-29 22:19:34 -0400

    Extract Subject ids

--- WebApp/static/js/app.js
+++ WebApp/static/js/app.js
@@ -1,6 +1,24 @@
 // Read Sample json data or log error
 d3.json("data/samples.json").then((sampleData) => {
-
+    var samples = sampleData.samples;
+    var otu_ids = samples.map(d => d.otu_ids);
+    var subject_ids = samples.map(d =>d.id);
+    console.log(subject_ids);
+    console.log(samples);
+    console.log(otu_ids);
+    var trace1 = {
+        x: samples.samples,
+        y: samples.samples,
+        type: "bar"
+      };
+      
+      var data = [trace1];
+      
+      var layout = {
+        title: "'Bar' Chart"
+      };
+      
+      Plotly.newPlot("test-bar", data, layout);
 
 
 }).catch(function(error) {
