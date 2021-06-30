64ae891ad9661a113345b4a1751dbd5c30f98e42
Andrew Anastasiades <atanasta.aa@gmail.com>
2021-06-29 21:56:08 -0400

    then load, catch error

--- WebApp/static/js/app.js
+++ WebApp/static/js/app.js
@@ -1,1 +1,8 @@
-var samples = d3.json("data/samples.json")
+// Read Sample json data or log error
+d3.json("data/samples.json").then((sampleData) => {
+
+
+
+}).catch(function(error) {
+    console.log(error);
+  });
