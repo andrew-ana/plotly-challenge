4aa4d54548b1ec86baf269e102da9d15ad107d74
Andrew Anastasiades <atanasta.aa@gmail.com>
2021-06-29 22:54:25 -0400

    Chart is dynamically loaded with subject id

--- WebApp/static/js/app.js
+++ WebApp/static/js/app.js
@@ -1,32 +1,50 @@
-
-// Read Sample json data or log error
 d3.json("data/samples.json").then((sampleData) => {
     var samples = sampleData.samples;
     var subject_ids = samples.map(d =>d.id);
-    console.log(subject_ids);
     // Let's dynamically fill the select with options
-    subject_select = d3.select()
+    var subject_select = d3.select("#selDataset")
     subject_ids.forEach(element => {
-        
+        subject_select.append("option").attr("value", element).html(element);
     });
-    var otu_ids = samples.map(d => d.otu_ids);
-    console.log(samples);
-    console.log(otu_ids);
-    var trace1 = {
-        x: samples.samples,
-        y: samples.samples,
-        type: "bar"
-      };
-      
-      var data = [trace1];
-      
-      var layout = {
-        title: "'Bar' Chart"
-      };
-      
-      Plotly.newPlot("test-bar", data, layout);
-
-
 }).catch(function(error) {
+    console.log("Could not fill select options");
     console.log(error);
+});
+
+
+
+
+
+
+function optionChanged(subject_id) {
+    console.log(subject_id);
+    // Read Sample json data or log error
+    d3.json("data/samples.json").then((sampleData) => {
+        var samples = sampleData.samples;
+        var subject_ids = samples.map(d =>d.id);
+        // Let's get this subject from the option selector
+        var this_sample = samples.filter(d => d.id == subject_id)[0];
+        console.log(this_sample)
+        var otu_ids = this_sample.otu_ids;
+        console.log("This Subject's ID")
+        console.log(this_sample.id);
+        console.log("This Subject's otu IDs")
+        console.log(otu_ids)
+        var trace1 = {
+            x: otu_ids,
+            y: samples.samples,
+            type: "bar"
+        };
+        
+        var data = [trace1];
+        
+        var layout = {
+            title: "'Bar' Chart"
+        };
+        
+        Plotly.newPlot("test-bar", data, layout);
+    }).catch(function(error) {
+        console.log(error);
   });
+
+}
